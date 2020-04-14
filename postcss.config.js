5faf7fc0a52c7e7b485d258dffb2c6282f8050ed
Will King <contact@wking.dev>
2020-04-14 10:17:53 -0500

    Adding the prefixes

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,10 @@
 module.exports = () => ({
-  plugins: [require("tailwindcss"), require("postcss-hexrgba")],
-})
+  plugins: [
+    require('tailwindcss'),
+    require('postcss-flexbugs-fixes'),
+    autoprefixer({
+      flexbox: 'no-2009',
+    }),
+    require('postcss-hexrgba'),
+  ],
+});
